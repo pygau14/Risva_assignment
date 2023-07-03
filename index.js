005f616dd4d0f1b9dac469077b8e951f4fcdc367
pygau14 <76560967+pygau14@users.noreply.github.com>
2023-07-03 23:55:53 +0530

    updated route.js

--- index.js
+++ index.js
@@ -40,6 +40,27 @@ function isAlphanumeric(str) {
   return hasAlphabets && hasNumbers;
 }
 
+// Sample data for fruits
+const fruits = [
+  { id: 1, name: 'Apple', color: 'Red' },
+  { id: 2, name: 'Banana', color: 'Yellow' },
+  { id: 3, name: 'Orange', color: 'Orange' },
+  { id: 4, name: 'Grapes', color: 'Green' },
+  { id: 5, name: 'Strawberry', color: 'Red' },
+  { id: 6, name: 'Blueberry', color: 'Blue' },
+  { id: 7, name: 'Pineapple', color: 'Yellow' },
+  { id: 8, name: 'Watermelon', color: 'Green' },
+  { id: 9, name: 'Mango', color: 'Yellow' },
+  { id: 10, name: 'Kiwi', color: 'Brown' },
+];
+
+// API endpoint to get sorted fruits by color
+app.get('/fruits', (req, res) => {
+  const sortedFruits = fruits.sort((a, b) => a.color.localeCompare(b.color));
+
+  res.json(sortedFruits);
+});
+
 // Start the server
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
